b7661d6f2253f969fec4dd8f0b3a013b59146bf1
bjornwilliam <bjorn.omholt@gmail.com>
2016-09-13 21:05:19 +0200

    Added spi driver, and ili lcd driver

---
+++ Drivers/spi.c
@@ -0,0 +1,178 @@
+#include "spi.h"
+#include "pmc.h"
+#include "pio.h"
+
+static void (*callBackFunctionPointer)(void); // Make a function pointer so that you can assign callback functions to it
+
+SemaphoreHandle_t spi_handlerIsDoneSempahore = NULL;
+SemaphoreHandle_t spi_mutex = NULL;
+
+static void spi_tranceive(uint32_t *transmit_buffer, uint8_t buffer_length, uint32_t *receive_buffer ) {
+	SPI->SPI_TPR = transmit_buffer; // Give address to tdr register
+	SPI->SPI_TCR = buffer_length;  // Give it length of transmit buffer
+	SPI->SPI_RPR = receive_buffer; // Give address to rpr register
+	SPI->SPI_RCR = buffer_length; // Give it length of receive buffer
+}
+
+void spi_freeRTOSTranceive(uint32_t  *transmit_buffer, uint8_t buffer_length, void (*callBackFunc)(void), uint32_t *receive_buffer ) {
+	//Aquire the spi resource
+	xSemaphoreTake(spi_mutex,portMAX_DELAY);
+	callBackFunctionPointer = callBackFunc;
+	spi_tranceive(transmit_buffer, buffer_length, receive_buffer);
+	// Wait for the SPI_Handler to run ,and signal that the transfer is complete
+	xSemaphoreTake(spi_handlerIsDoneSempahore, portMAX_DELAY);
+	
+	// Release spi resource
+	xSemaphoreGive(spi_mutex);
+}
+
+void SPI_Handler(void) {
+	long lHigherPriorityTaskWoken = pdFALSE;
+	SPI->SPI_SR; // MUST READ SR TO CLEAR NSSR
+	if (callBackFunctionPointer != NULL) {
+		callBackFunctionPointer();
+	}
+	if (spi_handlerIsDoneSempahore != NULL) {
+		xSemaphoreGiveFromISR(spi_handlerIsDoneSempahore,&lHigherPriorityTaskWoken);
+	}
+	portEND_SWITCHING_ISR(lHigherPriorityTaskWoken);
+}
+
+uint32_t spi_word(bool last_xfer,uint8_t chip_select, uint16_t data) {
+	uint32_t chip_select_correct_format;
+	switch(chip_select) {
+		case 0:
+		chip_select_correct_format = 0;
+		break;
+		case 1:
+		chip_select_correct_format = 1 << 16;
+		break;
+		case 2:
+		chip_select_correct_format = 3 << 16;
+		break;
+		case 3:
+		chip_select_correct_format = 7 << 16;
+		break;
+	}
+	if (last_xfer) {
+		return ( (1<<24) | chip_select_correct_format | data);
+	}
+	else {
+		return (chip_select_correct_format | data);
+	}
+}
+
+
+void spi_setBaudRateHz(uint32_t peripheral_clock_hz, uint32_t baud_rate_hz, uint8_t chip_select) {
+	
+	if ((peripheral_clock_hz/baud_rate_hz > 255) || ((peripheral_clock_hz/baud_rate_hz) < 1)) {
+		while(1);
+	}
+	else {
+		SPI->SPI_CSR[chip_select] |= (peripheral_clock_hz/baud_rate_hz)  << 8;
+	}
+}
+
+
+static void spi_setupMux(struct SpiMaster spi_settings) {
+	//Assigning SPI pin to correct peripheral
+	pio_setMux(PIOA, 14, A);//SPCK pin
+	pio_setMux(PIOA, 13, A);//MOSI pin
+	pio_setMux(PIOA, 12, A);//MISO pin
+	//NPCS0 pin
+	switch(spi_settings.cs_0){
+		case PA11:
+		pio_setMux(PIOA, 11, A);
+		break;
+		default:
+		break;
+	}
+	//NPCS1 pin
+	switch(spi_settings.cs_1) {
+		case PA9:
+		pio_setMux(PIOA, 9, B);
+		break;
+		case PA31:
+		pio_setMux(PIOA, 31, A);
+		break;
+		case PB14:
+		pio_setMux(PIOB, 14, A);
+		break;
+		case PC4:
+		pio_setMux(PIOC, 4, B);
+		break;
+		default:
+		break;
+	}
+	//NPCS2 pin
+	switch(spi_settings.cs_2){
+		case PA10:
+		pio_setMux(PIOA, 10, B);
+		break;
+		case PA30:
+		pio_setMux(PIOA, 30, B);
+		break;
+		case PB2:
+		pio_setMux(PIOB, 2, B);
+		break;
+		default:
+		break;
+	}
+	//NPCS3 pin
+	switch(spi_settings.cs_3){
+		case PA3:
+		pio_setMux(PIOA, 3, B);
+		break;
+		case PA5:
+		pio_setMux(PIOA, 5, B);
+		break;
+		case PA22:
+		pio_setMux(PIOA, 22, B);
+		break;
+		default:
+		break;
+	}
+}
+
+void spi_masterInit(struct SpiMaster SpiSettings) {
+	NVIC_DisableIRQ(SPI_IRQn);
+	NVIC_ClearPendingIRQ(SPI_IRQn);
+	NVIC_SetPriority(SPI_IRQn,SpiSettings.NVIC_spi_interrupt_priority);
+	NVIC_EnableIRQ(SPI_IRQn);
+	
+	spi_setupMux(SpiSettings);
+	pmc_enable_periph_clk(19); // Enable Spi clock
+	SPI->SPI_CR |= SPI_CR_SPIEN; // Enable SPI
+	SPI->SPI_MR |= SPI_MR_MSTR; // Set Master Mode
+	SPI->SPI_MR &= ~(1<<2); // Disable Chip select decode
+	SPI->SPI_MR |= 1<<1; // Variable peripheral select
+	SPI->SPI_MR &= ~(1<<4); // Disable mode fault detection
+	
+	SPI->SPI_IER |= 1<<8;  // NSSR interrupt enabled ( Chip select rising )
+	SPI->SPI_PTCR |= 1<<8; // Enable PDC transmit
+	SPI->SPI_PTCR |= 1<<0; // Enable PDC receive
+}
+void spi_deviceInit(struct SpiDevice SpiDeviceSettings) {
+	spi_setBaudRateHz(SpiDeviceSettings.peripheral_clock_hz,SpiDeviceSettings.spi_baudRate_hz,SpiDeviceSettings.chip_select);
+	switch (SpiDeviceSettings.spi_mode) {
+		case MODE_0:
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] &= ~(1<<0);
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= (1<<1);
+		break;
+		case MODE_1:
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] &= ~(1<<0);
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] &=  ~(1<<1);
+		break;
+		case MODE_2:
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= (1<<0);
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= (1<<1);
+		break;
+		case MODE_3:
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= (1<<0);
+		SPI->SPI_CSR[SpiDeviceSettings.chip_select] &= ~(1<<1);
+		break;
+	}
+	SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= (SpiDeviceSettings.bits_per_transfer-8) << 4;
+	SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= SpiDeviceSettings.time_until_first_valid_SPCK << 16;
+	SPI->SPI_CSR[SpiDeviceSettings.chip_select] |= SpiDeviceSettings.delay_between_two_consecutive_transfers << 24;
+}
---
+++ Drivers/spi.h
@@ -0,0 +1,123 @@
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "sam.h"
+#include "../FreeRTOS/include/FreeRTOS.h"
+#include "../FreeRTOS/include/semphr.h"
+
+#include <stdbool.h>
+
+extern SemaphoreHandle_t spi_handlerIsDoneSempahore;
+extern SemaphoreHandle_t spi_mutex;
+
+enum SpiMode{
+	MODE_0, // CPOL 0 , NCPHA 1
+	MODE_1, // CPOL 0 , NCPHA 0
+	MODE_2, // CPOL 1 , NCPHA 1
+	MODE_3  // CPOL 1 , NCPHA 0
+};
+
+enum NCPS0pin{none0, PA11};
+enum NCPS1pin{none1, PA9,	PA31,	PB14,	PC4};
+enum NCPS2pin{none2, PA10,	PA30,	PB2};
+enum NCPS3pin{none3, PA3,	PA5,	PA22};
+	
+struct SpiMaster {
+	uint8_t NVIC_spi_interrupt_priority; // must be >= 5
+	enum NCPS0pin cs_0;
+	enum NCPS1pin cs_1;
+	enum NCPS2pin cs_2;
+	enum NCPS3pin cs_3;
+};
+struct SpiDevice {
+	uint8_t chip_select;
+	uint32_t peripheral_clock_hz;
+	/*Specify Mode 0..3. Specified in datasheet at 35.7.2 */
+	enum SpiMode spi_mode;
+	/*Baud rate in Hz for all chip selects*/
+	uint32_t spi_baudRate_hz;
+	/*Bits per transfer for each chip select. 8 = 8 bits, 9 = 9 bits... 16 = 16 bits. */
+	uint8_t bits_per_transfer;
+	/* Time  after chip select activated until the first valid SPCK transition.*/
+	uint8_t time_until_first_valid_SPCK;
+	/* Delay  between two consecutive transfers on the same peripheral without removing the chip select.
+	If DLYBCT = 0, no delay is inserted. Look at page 818 for more information*/
+	uint8_t delay_between_two_consecutive_transfers;
+	};
+	
+void spi_masterInit(struct SpiMaster SpiSettings );
+void spi_deviceInit(struct SpiDevice SpiDeviceSettings);
+
+
+void spi_freeRTOSTranceive(uint32_t  *transmit_buffer, uint8_t buffer_length, void (*callBackFunc)(void), uint32_t *receive_buffer);
+uint32_t spi_word(bool last_xfer, uint8_t chip_select, uint16_t data);
+
+void spi_setBaudRateHz(uint32_t peripheral_clock_hz, uint32_t baud_rate_hz, uint8_t chip_select);
+
+
+/*
+FUNDAMENTALS:
+
+Setting the bit length of the spi register: 
+If you want to push out for example 16 bits on one spi transfer 
+you would set "bits_per_transfer[Chipselect] = 16".
+
+
+Initializing the spi:
+
+Here is an example of how to initialize the spi if you are only using chip select 0 on PA11.
+	struct SpiMaster SpiMasterSettings = {
+		.NVIC_spi_interrupt_priority = 10,
+		.cs_0 = PA11,
+		.cs_1 = none1,
+		.cs_2 = none2,
+		.cs_3 = none3
+	};
+	struct SpiDevice Spi0 = {
+		.chip_select = 0,
+		.bits_per_transfer = 8,
+		.delay_between_two_consecutive_transfers = 0,
+		.peripheral_clock_hz = 120000000,
+		.spi_baudRate_hz = 2000000,
+		.time_until_first_valid_SPCK = 0,
+		.spi_mode = MODE_0
+	};
+	spi_masterInit(SpiMasterSettings);
+	spi_deviceInit(Spi0);
+
+Chip selects: 
+The way the spi is configured the chip select will always remain low after a transfer until you either 
+start transferring to a different chip select or send a buffer with the last transfer bit set.
+
+The buffers that are given to the pdc registers work the following way:
+
+[xxxxxxx(7-bit) + LASTXFER(1-bit)(1)+ xxxx(4-bit) + PCS (4-bit) + DATA (8 to 16-bit)]
+
+The LASXFER bit tells the spi to deactivate the chipselect after the current data has been sent. 
+The PCS field is the chip select which can be set to 0-3 depending on which peripheral you want to send to.
+The data field will be from 8-16 bits depending on what you set the bitlength parameter to in the init.
+
+The user can construct an array of these buffers and then pass them onto the pdc. Example of constructing buffers:
+
+uint32_t tbuffer[3] = { 0x000(CS)(0..3) DATA1, 0x000(CS) DATA2, 0x01(LASTEXFER) 0 (CS) DATA3};
+
+The receive buffers must be the same size as the transmit buffer:
+uint32_t rbuffer[3] = {0};
+	
+There is a function included with the driver called spi_word which will create the words in the buffer for you:
+uint32_t tbuffer[3] = { spi_word(false, CS = 2, data1) , spi_word(false, CS = 2, data2), spi_word(true, CS = 2, data3)};
+	
+The received data will be the 8-16 LSB bits in the receive buffer index corresponding to the transmit buffer index.
+
+
+To use this driver you must create the binary sempahore and the mutex. These are declared extern for this purpose. This can be done before the schedueler starts. For example in main as follows:
+spi_handlerIsDoneSempahore = xSemaphoreCreateBinary();
+spi_mutex = xSemaphoreCreateMutex();
+
+Both binary sempahores and mutexes in freeRTOS are created so that the first call to semaphoretake will pass. This is unwanted behaviour for us
+since we want the spi_handlerIsDoneSempahore to be given from the SPI_Handler the first time. 
+
+The solution is to call xSemaphoreTake(spi_handlerIsDoneSempahore,0); in some task before the first spi transmission.
+*/
+
+#endif /* SPI_H_ */
